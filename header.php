a4a403ed162b5cb3f1b3632dd9074226cc9b3caa
Johnny Taylor <johnny@abledaccess.com>
2016-08-22 18:51:25 -0400

    Revert site-branding to its proposed state
    Revert .svg logo as well. And fix a .js file reorder bug Uglify
    presented that prevented the navigation button from expanding
    the navigation menu.

--- header.php
+++ header.php
@@ -29,21 +29,11 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding cf">
 
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" aria-label="Hashtag A Eleven Y T.O., Accessibility Toronto" rel="home">
+				<img class="inject-me logo" src="<?php echo get_template_directory_uri(); ?>/graphics/a11yto-logo.svg" alt="Logo: An illustration of a silhouette of the Toronto skyline centred in a blue and red border." />
 
-				<img class="inject-me logo" src="<?php echo get_template_directory_uri(); ?>/graphics/a11yto-logo.svg" alt="Logo: An illustration of a silhouette of the Toronto skyline centred in a blue and red border." aria-hidden="true" />
-
-				<?php
-				if ( is_front_page() && is_home() ) : ?>
-					<h1 class="site-title screen-reader-text" aria-hidden="true"><?php bloginfo( 'name' ); ?></h1>
-				<?php else : ?>
-					<p class="site-title screen-reader-text" aria-hidden="true"><?php bloginfo( 'name' ); ?></p>
-				<?php
-				endif;
-
-				$description = get_bloginfo( 'description', 'display' );
+				<?php $description = get_bloginfo( 'description', 'display' );
 				if ( $description || is_customize_preview() ) : ?>
-					<p class="site-description" aria-hidden="true"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+					<p class="site-description"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo $description; /* WPCS: xss ok. */ ?></a></p>
 				<?php
 				endif; ?>
 
